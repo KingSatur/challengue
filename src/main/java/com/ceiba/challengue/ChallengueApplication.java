7308fe2de23fdc1909fdcb18a86fac0740d17f5f
Juan David Campillo Cepeda <juancawool@outlook.com>
2021-05-19 18:05:13 -0500

    Sample

--- src/main/java/com/ceiba/challengue/ChallengueApplication.java
+++ src/main/java/com/ceiba/challengue/ChallengueApplication.java
@@ -1,13 +1,29 @@
 package com.ceiba.challengue;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChallengueApplication {
+	
 
-	public static void main(String[] args) {
+	private SystemOperatorAgent agent;
+	
+
+	public static void main(String[] args) throws ParseException, IOException, InterruptedException {
 		SpringApplication.run(ChallengueApplication.class, args);
+		final Process dateProcess = Runtime.getRuntime().exec("cmd /c date 19-05-21");
+	
 	}
+
 
 }
---
+++ src/main/java/com/ceiba/challengue/SystemOperatorAgent.java
@@ -0,0 +1,45 @@
+package com.ceiba.challengue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SystemOperatorAgent {
+
+	
+	public void executeCommand(String command) {
+	    try {
+	        log(command);
+	        Process process = Runtime.getRuntime().exec(command);
+	        logOutput(process.getInputStream(), "");
+	        logOutput(process.getErrorStream(), "Error: ");
+	        process.waitFor();
+	    } catch (IOException | InterruptedException e) {
+	    	System.out.println(e.getMessage());
+	        e.printStackTrace();
+	    }
+	}
+
+	private void logOutput(InputStream inputStream, String prefix) {
+	    new Thread(() -> {
+	        Scanner scanner = new Scanner(inputStream, "UTF-8");
+	        while (scanner.hasNextLine()) {
+	            synchronized (this) {
+	                log(prefix + scanner.nextLine());
+	            }
+	        }
+	        scanner.close();
+	    }).start();
+	}
+
+	private static SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss:SSS");
+
+	private synchronized void log(String message) {
+	    System.out.println(format.format(new Date()) + ": " + message);
+	}
+}
