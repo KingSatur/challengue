c093d9677f75840c29c176f4f65832e62b23e9f8
Juan David Campillo Cepeda <juancawool@outlook.com>
2021-05-18 23:26:07 -0500

    Initial commit, project setup with spring initialzr

---
+++ src/main/java/com/ceiba/challengue/ChallengueApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.challengue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengueApplication.class, args);
+	}
+
+}
